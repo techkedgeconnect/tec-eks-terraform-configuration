1bdb5aa485fb6387ea1411585491f31d7d277e31
Samson Wahab <techkedgeconnect@gmail.com>
2024-10-14 09:42:50 +0100

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,15 @@
+# Define terraform version and required provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  profile = var.aws_profile
+  region  = var.aws_region
+}
